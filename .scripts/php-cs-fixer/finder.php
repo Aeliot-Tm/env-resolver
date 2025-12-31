186b8d6d82f78e70a59ce298100cb696585e724f
Anatoliy Melnikov <5785276@gmail.com>
2026-01-01 01:52:18 +0300

    Fix config of checkers

--- .scripts/php-cs-fixer/finder.php
+++ .scripts/php-cs-fixer/finder.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 return $finder = (new PhpCsFixer\Finder())
     ->files()
     ->ignoreVCS(true)
-    ->in(dirname(__DIR__, 2))
-    ->exclude(['tests/fixtures', 'var', 'vendor'])
-    ->append([dirname(__DIR__, 2) . '/bin/validate']);
+    ->in([dirname(__DIR__, 2), dirname(__DIR__, 2) . '/.scripts'])
+    ->exclude(['tests/fixtures', 'var', 'vendor']);
 
